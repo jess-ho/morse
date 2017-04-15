642a6c09a5bb14940c244fe1fad5a057187aaec2
jess-ho <jessica.ho@live.com>
2017-04-14 20:05:18 -0400

    translation completed

--- src/scripts/components/create.js
+++ src/scripts/components/create.js
@@ -7,21 +7,55 @@ export default class Create extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			code: [],
-			letter: []
+			text: '',
+			codes: []
 		}
+
+		this.myKeyPress = this.myKeyPress.bind(this)
+	}
+	myKeyPress(e) {
+		// convert string of this.state.text into an array where each letter is separated
+		const textSplit = e.target.value.replace(/[^\w]/gi, ' ').split('')
+		// console.log(textSplit)
+
+		// loop through the array and match against database
+		const textArray = []
+		const charList = Object.keys(morse)
+
+		for (var i = 0; i < textSplit.length; i++) {
+			for (var j = 0; j < charList.length; j++) {
+				if (textSplit[i] === charList[j]) {
+					textArray.push(morse[charList[j]])
+				} 
+				// else if (textSplit[i] === " ") {
+				// 	textArray.push("")
+				// }
+			}
+
+			if (textSplit[i] === " ") {
+				textArray.push('/')
+			}
+		}
+
+		// put enter as new line
+		// console.log(textArray)
+
+		// set state for both at once, updates in real time? iunno something like that
+		this.setState({
+			[e.target.name]: e.target.value,
+			codes: textArray.join(' ')
+		})
+
+		// console.log(e.target.value)
+		// console.log(textInput)
 	}
 	render() {
-		document.getElementsByClassName('message')[0].addEventListener('keyDown', (e) => {
-			if (e.keyCode >= 65 && e.keyCode <= 90) {
-
-			}
-		})
 		return (
 			<div className="message">
-				{/* two states, key listeners */}
-				<div></div>
-				<div></div>
+				<textarea name="text" id="" cols="30" rows="10" value={this.state.text} onChange={this.myKeyPress}></textarea>
+				<div>
+					{this.state.codes}
+				</div>
 			</div>
 		)
 	}
